40a2225f16568059e7440b03a1c7acf8bf3d43cd
Andrea Vaccari <iz1emnand@gmail.com>
2026-06-17 17:07:10 +0200

    gitignore

--- build/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/BlendMode.cpp.o: \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/src/SFML/Graphics/BlendMode.cpp \
- /usr/include/stdc-predef.h \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/include/SFML/Graphics/BlendMode.hpp \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/include/SFML/Config.hpp
--- build/_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Window/CMakeFiles/sfml-window.dir/Unix/KeySymToKeyMapping.cpp.o: \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.cpp \
- /usr/include/stdc-predef.h \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/src/SFML/Window/Unix/KeySymToKeyMapping.hpp \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/include/SFML/Window/Keyboard.hpp \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/include/SFML/Window/Export.hpp \
- /home/andrea/Documenti/UNI/computer\ grafica/Sistema_Solare/SistemaSolare_Vaccari/build/_deps/sfml-src/include/SFML/Config.hpp \
- /usr/include/X11/X.h /usr/include/X11/keysym.h \
- /usr/include/X11/keysymdef.h
